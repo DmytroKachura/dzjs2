be0ee64100b2187b1a5afa7bc5419d7872d5ce25
Dmytro Kachura <dkachura1999@gmail.com>
2017-02-27 11:59:13 +0000

    a

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var a = 1;
 var b = 1;
 var c;
-var fib = prompt("Enter");
+var fib = prompt("Enter the number of fibonacci, which you want to see");
 document.write(a + "<br>");
 document.write(b + "<br>");
 
